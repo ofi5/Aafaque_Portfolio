54e0f7574545d10f8a5f5cc1ed2e0b83febc5b21
ofi5 <aafaque123@yahoo.com>
2025-08-21 17:10:41 -0700

    added image

--- src/components/intro/intro-content/index.jsx
+++ src/components/intro/intro-content/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import girl from "../../../images/girl.png"
+import aafaquerasheed from "../../../images/aafaquerasheed.jpeg"
 import hand from "../../../images/hand.png";
 import utdsmall from "../../../images/utdsmall.png"
 
@@ -13,45 +13,41 @@ import mahe from '../../../images/portfolio/mahe.png'
 
 const IntroContent = () => {
   return (
-    
     <div className="intro-content">
         <div className="layout">
             <div className="left-col">
               <h1 className='title'>
-                  
-                  
-             
-
                   <span className="big-text">
                   Welcome to my Portfolio
                   </span>
                 </h1>    
                 <p className='myintro'>
-                My name is Aafaque Rasheed and I am currently pursuing Masters at University of Texas, Dallas in Information, Technology and Management. I am an aspiring fullstack developer and heres a showcase of my projects.
+                I'm Aafaque Rasheed, a full-stack developer with expertise in React.js, Node.js, and modern cloud technologies. I'm passionate about building scalable web applications and currently expanding my skills into Generative AI. I've developed and deployed multiple projects achieving 40% performance improvements and delivering solutions ahead of schedule. This portfolio showcases my learning journey, technical projects, and development capabilities.
                 </p>
-                
-                {/* <CallToAction text="Contact Me" action = {()=>scrollToSection("contact")}/> */}
             </div>
-            
             <div className="right-col">
-              {/* <img src={utdLogo}/> */}
-             <div className='highlights vertical'>
-              {/* <div className="highlights horizontal"> */}
-                <div className="icon">
-                  <img src={utdsmall} alt="utdLogo" />
-                </div>
-                <div className="text"> Masters in Information, Technology and Management</div>
+              {/* Circular Profile Image */}
+              <div className="profile-image-container">
+                <img src={aafaquerasheed} alt="Aafaque Rasheed" className="profile-image" />
               </div>
-              <div className="highlights vertical">
-                <div className="icon">
-                <img src={mahe} alt="mahe" />
+              
+              {/* Education Highlights - Moved Lower */}
+              <div className="education-highlights">
+                <div className='highlights vertical'>
+                  <div className="icon">
+                    <img src={utdsmall} alt="utdLogo" />
+                  </div>
+                  <div className="text"> Masters in Information, Technology and Management</div>
                 </div>
-                <div className="text">
-                  <span>Bachelors of Technology in Civil Engineering</span>
-                 
+                <div className="highlights vertical">
+                  <div className="icon">
+                  <img src={mahe} alt="mahe" />
+                  </div>
+                  <div className="text">
+                    <span>Bachelors of Technology in Civil Engineering</span>
+                  </div>
                 </div>
               </div>
-
             </div>
         </div>
     </div>
